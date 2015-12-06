86bed17cf524b852f2b7249bd3092d486f28b651
Ziggy Lotivio <dook@stardust.red>
2015-12-05 21:58:17 -0500

    Delayed GET request by 1 second per request, and added exponential delay algorithm for DeviantArt API

--- routes.js
+++ routes.js
@@ -4,11 +4,15 @@ var request = require("request");
 var promise = require("promise");
 var filter  = require("./filter.js");
 
+var global_delay = 1000; // 1 second
+
 var client_keys = {
   "deviantart": {
     client_id: "4010",
     client_secret: "ca8edec4cdaa48fc6b64196de7394d66",
-    access_token: null
+    access_token: null,
+    slow_down: false,
+    last_delay: 0
   }
 };
 (function newToken() {
@@ -41,62 +45,67 @@ router.get('/request', function(req, response, next) {
   var   page     = req.query.page || 0;
   var  limit     = req.query.limit || 24;
 
+  // 24 is set hard limit due to Deviantart...
   if(limit > 24) limit = 24;
 
-  var deviant_art_promise = new promise(function(resolve, reject) {
-    request({
-      url: "https://www.deviantart.com/api/v1/oauth2/browse/tags" +
-           "?tag="    +  req.query.tags.replace(/(%20|\ ).*$/, "") +
-           "&offset=" +  page*limit +
-           "&limit="  +  limit,
-      method:   "GET",
-      headers: { "Authorization": "Bearer " + client_keys["deviantart"].access_token }
-    }, function(err, res, body) {
-      // Normalize deviantart data before resolving promise
-      var retval = JSON.parse(body);
-      var new_body = retval.results || [];
-      new_body = filter.deviantart(new_body);
-      resolve(new_body);
+  setTimeout(function() { // Global delay, always wait 1 second (at least)
+    // Deviantart has adaptive rate limiting...
+    var deviantart_promise = new promise(function(resolve, reject) {
+      setTimeout(function() {
+        request({
+          url: "https://www.deviantart.com/api/v1/oauth2/browse/tags" +
+               "?tag="    +  req.query.tags.replace(/(%20|\ ).*$/, "") +
+               "&offset=" +  page*limit +
+               "&limit="  +  limit,
+          method:   "GET",
+          headers: { "Authorization": "Bearer " + client_keys["deviantart"].access_token }
+        }, function(err, res, body) {
+          if(res.statusCode === 429) { // Reached adaptive rate limit
+            client_keys["deviantart"].last_delay *= 2;
+          } else {
+            if(--client_keys["deviantart"].last_delay <= 0) {
+              // Deactivate slow_down limit
+              client_keys["deviantart"].last_delay = 0;
+            }
+          }
+
+          // Normalize deviantart data before resolving promise
+          var retval = JSON.parse(body);
+          var new_body = retval.results || [];
+          new_body = filter.deviantart(new_body);
+          resolve(new_body);
+        });
+      }, client_keys["deviantart"].last_delay);
     });
-  });
 
-  //deviant_art_promise.then(
-    //function success(res) { response.json(res); },
-    //function error(res)   { response.json(res); }
-  //);
+    var e926_promise = new promise(function(resolve, reject) {
+      request({
+        url: "https://e621.net/post/index.json" +
+             "?tags="       + req.query.tags     +
+             "%20rating:s"  + // Force safe rating search
+             "%20score:>40" + // Force high score...
+             "%20-friendship_is_magic" + // Substract ponies...
+             "&page="       + page  +
+             "&limit="      + limit,
+        method:   "GET"
+      }, function(err, res, body) {
+        // Normalize e621 data before resolving promise
+        var retval = JSON.parse(body);
+        var new_body = retval || [];
+        new_body = filter.e926(new_body);
 
-  var e926_promise = new promise(function(resolve, reject) {
-    request({
-      url: "https://e621.net/post/index.json" +
-           "?tags="       + req.query.tags     +
-           "%20rating:s"  + // Force safe rating search
-           "%20score:>40" + // Force high score...
-           "%20-friendship_is_magic" + // Substract ponies...
-           "&page="       + page  +
-           "&limit="      + limit,
-      method:   "GET"
-    }, function(err, res, body) {
-      // Normalize e621 data before resolving promise
-      var retval = JSON.parse(body);
-      var new_body = retval || [];
-      new_body = filter.e926(new_body);
-
-      resolve(new_body);
+        resolve(new_body);
+      });
     });
-  });
 
-  //e926_promise.then(
-    //function success(res) { response.json(res); },
-    //function error(res)   { response.json(res); }
-  //);
-
-  promise.all([ deviant_art_promise, e926_promise ]).then(
-    function(res) {
-      //response.end(res[0].concat(res[1]));
-      var retval = res[0].concat(res[1]);
-      response.json(retval);
-    }
-  );
+    promise.all([ deviantart_promise, e926_promise ]).then(
+      function(res) {
+        //response.end(res[0].concat(res[1]));
+        var retval = res[0].concat(res[1]);
+        response.json(retval);
+      }
+    );
+  }, global_delay);
 
 });
 
