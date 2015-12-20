49e4725ed8c72ebb222343a3471a8c13321a1ac6
Ziggy Lotivio <dook@stardust.red>
2015-12-19 20:17:40 -0500

    Added HTML and CSS minifiers

--- browserify.sh
+++ browserify.sh
@@ -1,3 +1,14 @@
+# mini HTML
+node_modules/.bin/html-minifier \
+--remove-comments --collapse-whitespace \
+--collapse-boolean-attributes --remove-redundant-attributes \
+--use-short-doctype --remove-empty-attributes --remove-optional-tags \
+app/index.html > public/index.html
+
+#minify CSS
+node_modules/.bin/cleancss \
+-o public/css/index.min.css app/css/index.css
+
 # Uglify initial files
 node_modules/.bin/browserify \
 app/js/angular.js \
@@ -11,4 +22,4 @@ node_modules/.bin/browserify \
 app/js/bootstrap.min.js \
 app/js/bootstrap-toolkit.min.js \
 app/js/index.js \
-| node_modules/.bin/uglifyjs > public/js/index.js;
+| node_modules/.bin/uglifyjs > public/js/index.min.js;
