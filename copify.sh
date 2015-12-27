5d816aa4c3ade816656e1865598734d029ab3c1e
Ziggy Lotivio <dook@stardust.red>
2015-12-27 01:43:56 -0500

    Removed files in public directory, leave empty from now on

--- copify.sh
+++ copify.sh
@@ -1,7 +1,7 @@
-# mini HTML
+# copy HTML
 cat app/index.html > public/index.html
 
-# minify CSS
+# concatenate CSS
 cat \
 app/css/bootstrap.min.css \
 app/css/bootstrap-theme.min.css \
@@ -9,7 +9,16 @@ app/css/index.css \
 app/css/rzslider.min.css \
 > public/css/index.min.css
 
+# copy fonts
+cp -r app/fonts/* public/fonts
+
+# copy images
+cp -r app/img/* public/img
+
 # Copy plugins;
+cp app/js/angular.min.js public/js/angular.min.js
+cp app/js/angular-route.min.js public/js/angular-route.min.js
+cp app/js/angular-touch.min.js public/js/angular-touch.min.js
 cp app/js/fastclick.js public/js/fastclick.js
 cp app/js/rzslider.js public/js/rzslider.js
 
