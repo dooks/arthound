0d5242be4fd963f8543810e006162ebecdc4f2f5
Ziggy Lotivio <dook@stardust.red>
2015-12-31 03:21:27 -0500

    Fixed browserify index.js concatenation bug

--- browserify.sh
+++ browserify.sh
@@ -36,13 +36,13 @@ app/js/angular.js \
 app/js/services.js \
 app/js/controller.js \
 app/js/directives.js \
-| node_modules/.bin/uglifyjs > app/js/index.js;
+| node_modules/.bin/uglifyjs > public/js/index.js;
 
 # concatenate rest of JS files
 node_modules/.bin/browserify \
 app/js/bootstrap.min.js \
 app/js/bootstrap-toolkit.min.js \
-app/js/index.js \
+public/js/index.js \
 | node_modules/.bin/uglifyjs > public/js/index.min.js;
 
 
